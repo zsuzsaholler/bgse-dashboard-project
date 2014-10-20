116b9d79cab21d23c862ba4028ee1f2f7e5af85b
Christian <ctbrownlees@gmail.com>
2014-10-20 21:15:40 +0200

    setting up db and installation script

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# installion script
+
+cmd=$1
+
+user=`grep dbuser service.conf | cut -f2 -d' '`
+pswd=`grep dbpswd service.conf | cut -f2 -d' '`
+
+case $cmd in
+
+install)
+	echo "Installing"
+
+	mysql -u $user -p$pswd < db/ecommerce.sql
+	mysql -u $user -p$pswd < data/ecommerce-dump.sql
+
+	#mkdir -p "$HOME/public_html/MyApp"
+	#cp -rf web/* "$HOME/public_html/MyApp"
+
+	echo "done!"
+	;;
+
+uninstall)
+	echo "Uninstalling"
+	
+	mysql -u $user -p$pswd -e "DROP DATABASE ecommerce;" 
+	#rm -rf "$HOME/public_html/MyApp"
+
+	echo "done!"
+	;;
+
+*)
+	echo "Unknown Command!"
+
+esac
