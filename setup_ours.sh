9625a7f996d28aa8f80004d628e5ffa8f1327fe7
zsuzsaholler <holler.zsuzsa@gmail.com>
2015-12-03 00:17:41 +0100

    regression.R added, modified version of setup.sh added, some other files are moved to other directories

---
+++ setup_ours.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# installion script
+
+cmd=$1
+
+user=`grep dbuser service.conf | cut -f2 -d' '`
+pswd=`grep dbpswd service.conf | cut -f2 -d' '`
+
+case $cmd in
+
+install)
+	echo "Installing"
+
+	mysql -u $user -p$pswd < db/Schema_creation.sql
+	mysql -u $user -p$pswd < data/dump.sql
+	mysql -u $user -p$pswd < analysis/Cleaning.sql
+
+	mkdir -p "/var/www/html/MyApp"
+	cp -rf web/* "/var/www/html/MyApp"
+
+	echo "done!"
+	;;
+
+uninstall)
+	echo "Uninstalling"
+	
+	mysql -u $user -p$pswd -e "DROP DATABASE mydb;" 
+	rm -rf "/var/www/html/MyApp"
+
+	echo "done!"
+	;;
+
+run)
+	echo "Running"
+	R CMD BATCH analysis/analysis.R 
+	cat analysis.Rout
+	rm analysis.Rout
+	cp web/categories_network.png "/var/www/html/MyApp"
+
+	;;
+
+*)
+	echo "Unknown Command!"
+
+esac
