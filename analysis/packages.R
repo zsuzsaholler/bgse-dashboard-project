94df79b181c8f038004daf4ad3b711021dd8ef44
zsuzsaholler <holler.zsuzsa@gmail.com>
2015-12-16 22:43:12 +0100

    Modifications to packages.R.

--- analysis/packages.R
+++ analysis/packages.R
@@ -1,11 +1,11 @@
 # Install R packages used by the application at runtime.
 # This can require more memory than the smallest AWS instance provides.
 
-install.packages("RMySQL", repos="http://cran.r-project.org")
-install.packages("igraph", repos="http://cran.r-project.org")
-install.packages("dplyr", repos="http://cran.r-project.org")
-install.packages("stringr", repos="http://cran.r-project.org")
-install.packages("e1071", repos="http://cran.r-project.org")
-install.packages("sandwich", repos="http://cran.r-project.org")
-install.packages("lmtest", repos="http://cran.r-project.org")
-install.packages("wordcloud", repos="http://cran.r-project.org")
+install.packages("RMySQL")
+install.packages("igraph")
+install.packages("dplyr")
+install.packages("stringr")
+install.packages("e1071")
+install.packages("sandwich")
+install.packages("lmtest")
+install.packages("wordcloud")
